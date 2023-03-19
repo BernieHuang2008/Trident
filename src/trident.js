6a52e43b5830e26baaa09146049f7348063287a9
berniehuang2008 <berniehuang2008@163.com>
2023-03-19 14:08:19 +0800

    update

--- src/trident.js
+++ src/trident.js
@@ -1,27 +1,152 @@
 // generate a class called Trident
 
+const PT = 0.376; // pt -> mm
+
+class TridentPen {
+	constructor(div, config) {
+		this.div = div; // div is a HTMLDivElement which act as a container
+		this.config = config; // config is a dictionary object
+		this.pages = []; // store all pages in this document (canvas)
+	}
+
+	new_page(args) {
+		var canvas = document.createElement("canvas");
+		canvas.innerText = "You Browser doesn't support CANVAS!";
+
+		canvas.width = this.config.zoom_ratio * this.config.high_dpi * this.config.paper_size[0];
+		canvas.height = this.config.zoom_ratio * this.config.high_dpi * this.config.paper_size[1];
+
+		canvas.style.marginTop = "10px";
+		canvas.style.marginBottom = "10px";
+		canvas.style.zoom = 1 / this.config.high_dpi;
+
+		canvas.id = this.pages.length;
+		this.div.appendChild(canvas);
+		this.pages.push(canvas);
+
+
+		if (this.config.pen.page_border) {
+			canvas.style.border = `solid ${this.config.pen.page_border} 1px`;
+		}
+
+		if (this.config.format_page) {
+			this.config.format_page.forEach(command => {
+				var cmd_para = command[1];
+				var cmd_name = command[0];
+				this.config.pen.pos[0] = this.pages.length - 1;
+				this[cmd_name](cmd_para);
+			});
+		}
+
+		this.move_pen([0, 0]);
+	}
+
+	move_pen([new_x, new_y]) {
+		this.config.pen.pos[1] = this.config.margin[0] + new_x;
+		this.config.pen.pos[2] = this.config.margin[2] + new_y;
+	}
+
+	_move_pen([new_x, new_y]) {
+		// no margin
+		this.config.pen.pos[1] = new_x;
+		this.config.pen.pos[2] = new_y;
+	}
+
+	_ctx() {
+		const [page, x, y] = this.config.pen.pos;
+		const canvas = this.pages[page];
+		const ctx = canvas.getContext('2d');
+		ctx.fillStyle = this.config.pen.color;
+		ctx.scale(this.config.zoom_ratio * this.config.high_dpi, this.config.zoom_ratio * this.config.high_dpi);
+		return [x, y, ctx];
+	}
+
+	set_color([color]) {
+		this.config.pen.color = color;
+	}
+
+	draw_text([text, font_size]) {
+		var [x, y, ctx] = this._ctx();
+
+		ctx.font = `${font_size*0.25}pt ` + this.config.pen.font;
+		ctx.fillText(text, x, y);
+	}
+
+	draw_arc([radius, startAngle, endAngle, anticlockwise]) {
+		var [x, y, ctx] = this._ctx();
+
+		ctx.arc(x, y, radius, startAngle, endAngle, Boolean(anticlockwise));
+	}
+
+	draw_rect([width, height]) {
+		var [x, y, ctx] = this._ctx();
+
+		ctx.strokeRect(x, y, width, height);
+	}
+
+	fill_rect([width, height]) {
+		var [x, y, ctx] = this._ctx();
+
+		ctx.fillRect(x, y, width, height);
+	}
+
+	clear_rect([width, height]) {
+		ctx.clearRect(x, y, width, height);
+	}
+
+	svg_stroke([path_str]) {
+		var [x, y, ctx] = this._ctx();
+
+		var p = new Path2D(`M${x} ${y} ` + path_str);
+		ctx.stroke(p);
+	}
+
+	svg_fill([path_str]) {
+		var [x, y, ctx] = this._ctx();
+
+		var p = new Path2D(`M${x} ${y} ` + path_str);
+		ctx.fill(p);
+	}
+
+	_eval([cmd]) {
+		var [x, y, ctx] = this._ctx();
+
+		eval(cmd);
+	}
+
+	static a4 = {
+		zoom_ratio: 1, // change to match your screen size (auto calculated in Trident).
+		high_dpi: 5, // Scale for high resulution screen. --- Scale 5 times, and then zoom out 1/5 times, so you get a high-resolution image.
+		paper_size: [210, 297], // [width, height] mm
+		margin: [20, 20, 20, 20], // [left, right, top, bottom] mm
+		pen: {
+			page_border: false, // decide whether to draw page border
+			pos: [0, 0, 0], // [page, width(x), height(y)] mm
+			color: "black", // pen's color
+			font: "Times New Roman", // pen's font
+
+		},
+		format_page: [
+			["_move_pen", [20, 12]],
+			// ["fill_rect", [170, 1 * PT]],
+		]
+	}
+}
+
 class Trident {
 	constructor(div) {
 		this.div = div;
 		this.div.innerHTML = "";
 		this.config = {
 			resolution: [window.innerWidth, window.innerHeight], // [browser_width, browser_height]px
-			zoom_ratio: Math.min(window.innerWidth / 297, window.innerHeight / 210),
+			zoom_ratio: Math.min(window.innerWidth / 210, window.innerHeight / 297),
 
 			// Refer to the 'geometry' package, see https://www.overleaf.com/learn/latex/Page_size_and_margins for detail.
-			paper_size: [297, 210], // [width, height] mm
+			paper_size: [210, 297], // [width, height] mm
 			margin: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
 				0
 			] // [textwidth, textheight, total, left, lmargin, inner, right, rmargin, outer, top, tmarginbottom, bmargin, headheight, headsep, footnotesep, footskip, marginparwidth, marginpar] mm
 		}
-	}
-
-	_new_page() {
-		var canvas = document.createElement("canvas");
-		canvas.innerText = "You Browser doesn't support CANVAS!";
-		canvas.width = this.config.zoom_ratio * this.config.paper_size[0];
-		canvas.height = this.config.zoom_ratio * this.config.paper_size[1];
-		this.div.appendChild(canvas);
 	}
 
 	_normalizeCode(code) {
@@ -55,7 +180,7 @@ class Trident {
 			[/\s+/g, ' '], // replace multiple spaces with single space again
 		].forEach(x => {
 			if ((typeof x[1]) == 'string')
-				code = code.replaceAll(x[0], ' '+x[1] + ' ');
+				code = code.replaceAll(x[0], ' ' + x[1] + ' ');
 			else
 				code = code.replaceAll(x[0], x[1]);
 		})
@@ -76,7 +201,7 @@ class Trident {
 			while (1) {
 				if (code[i].match(command_char) == null && brackets == '')
 					break;
-				if (bracketed && brackets=='' && !code[i].match(command_bracket))
+				if (bracketed && brackets == '' && !code[i].match(command_bracket))
 					break;
 				command += code[i];
 				if (code[i].match(command_bracket)) {
